ebd02c0fa88731961b98317dcd599bdcf81d1287
Adrian Ortega <adrian.ortega22@gmail.com>
2025-12-20 00:37:18 -0800

    adds smoke test

---
+++ apps/server/src/smoke.test.ts
@@ -0,0 +1,9 @@
+// apps/api/src/smoke.test.ts
+
+import { expect, describe, it } from '@jest/globals';
+
+describe("smoke test", () => {
+  it("always passes", () => {
+    expect(true).toBe(true);
+  });
+});
