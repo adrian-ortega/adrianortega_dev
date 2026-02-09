63d73f326c58270847ac3e60d8b7543f76d0fc02
Adrian Ortega <adrian.ortega22@gmail.com>
2026-02-09 00:52:23 -0800

    adds sentry

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,13 @@
 import "dotenv/config";
 
+import Sentry from "@sentry/node";
+
 import { startServer } from "./server";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  sendDefaultPii: true,
+});
 
 startServer().catch((e) => {
   // eslint-disable-next-line no-console
