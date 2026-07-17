2b8c0d452253ed7e812ad41c9096226ed17753f2
Adrian Ortega <adrian.ortega22@gmail.com>
2026-07-16 19:16:51 -0700

    updates frontend

--- apps/server/src/controllers/postsController.ts
+++ apps/server/src/controllers/postsController.ts
@@ -1,6 +1,8 @@
 import type { Request, Response, NextFunction } from "express";
 import { listPostsByCategory, readPostFile } from "../services/postsService";
 import { makeSlugController } from "./entityController";
+import { readTagFile } from "../services/tagsService";
+import { type TagEntity } from "../../../shared/types";
 
 export async function listPosts(_req: Request, res: Response, next: NextFunction) {
   try {
@@ -11,7 +13,13 @@ export async function listPosts(_req: Request, res: Response, next: NextFunction
       error = `No posts found with tag "${tag}"`;
     }
 
-    return res.json({ posts, error });
+    const meta: { tag?: TagEntity | null } = { tag: null };
+
+    if (tag) {
+      meta.tag = await readTagFile(tag as string)
+    }
+
+    return res.json({ posts, meta, error });
   } catch (e) {
     return next(e);
   }
